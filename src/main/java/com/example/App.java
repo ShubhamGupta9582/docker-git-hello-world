4f4db202972a5b8af005d1e13fd2e33efce2a66a
Shubham Gupta <shubhamgupta9582@gmail.com>
2019-11-22 18:39:05 +0530

    msg changed

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,7 +4,7 @@ package com.example;
 public class App {
     public static void main( String[] args ) throws Exception {
         for (int i=0; i<100; i++) {
-            System.out.println( "Hello World Ping " + i);
+            System.out.println( "Hi Ping " + i);
             Thread.sleep(1000);
         }
     }
